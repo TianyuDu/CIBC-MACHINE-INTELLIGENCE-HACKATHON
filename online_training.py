2ec510c952a6e2680be7263fb7b3e2e608e8e4ac
MasterDoo <masterdu9951@icloud.com>
2018-09-21 11:58:37 -0400

    (tianyu) add online_training script

---
+++ online_training.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import keras
+import numpy as np
+import pandas as pd
+
+
+def save_model(model, file_dir: str=None) -> None:
+    # Try to create record folder.
+    try:
+        folder = f"./saved_models/{file_dir}/"
+        os.system(f"mkdir {folder}")
+        print(f"Experiment record directory created: {folder}")
+    except:
+        print("Current directory: ")
+        _ = os.system("pwd")
+        raise FileNotFoundError(
+            "Failed to create directory, please create directory ./saved_models/")
+    
+    # Save model structure to JSON
+    print("Saving model structure...")
+    model_json = model.to_json()
+    with open(f"{folder}model_structure.json", "w") as json_file:
+        json_file.write(model_json)
+    print("Done.")
+
+    # Save model weight to h5
+    print("Saving model weights...")
+    model.save_weights(f"{folder}model_weights.h5")
+    print("Done")
+
+    # Save model illustration to png file.
+    print("Saving model visualization...")
+    keras.utils.plot_model(
+        model,
+        to_file=f"{folder}model.png",
+        show_shapes=True, 
+        show_layer_names=True)
+    print("Done.")
+
+
+def load_model() -> keras.Sequntial():
+    pass
